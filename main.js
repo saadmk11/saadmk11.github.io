850282b17a651617167bc7b0f8fcc5534a3f2835
saadmk11 <saad.mk112@gmail.com>
2020-08-12 12:54:32 +0600

    Add github actions to update repo data

---
+++ main.js
@@ -0,0 +1,48 @@
+function ajaxGET(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                return;
+            }
+            callback(data);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var container = document.getElementById("openSourceRepos");
+
+    ajaxGET('repo_data.json', function(data) {
+        data.forEach(function (repo) {
+            var child = `
+                <div class="col-sm-6 pb-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6 class="card-title">
+                                <i class="fa fa-book text-primary"></i>
+                                <a href="${repo.url}" target="_blank">
+                                    <b>${repo.full_name}</b>
+                                </a>
+                            </h6>
+                            <p class="card-text">${repo.short_description}</p>
+                            <p class="card-text">
+                                <span class="pr-4"><i class="fa fa-circle text-primary pr-2" aria-hidden="true"></i> ${repo.main_programing_language}</span>
+                                <span class="pr-4"><i class="fa fa-star-o text-warning pr-2" aria-hidden="true"></i> ${repo.stars}</span>
+                                <span class="pr-4"><i class="fa fa-code-fork text-success pr-2" aria-hidden="true"></i> ${repo.forks}</span>
+                                <span class="pr-4"><i class="fa fa-eye text-dark pr-2" aria-hidden="true"></i> ${repo.watchers}</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            `;
+            container.insertAdjacentHTML('beforeend', child);
+        });
+    });
+});
